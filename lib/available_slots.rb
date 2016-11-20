76553c74fd5d693f9272ca088c9ddb1cc3f06ff7
MaxProvin <maxprovin01@gmail.com>
2016-11-20 22:05:11 +0000

    available slots has a method to find closest apointment time

--- lib/available_slots.rb
+++ lib/available_slots.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'byebug'
 
 class AvailableSlots
 
@@ -10,10 +11,25 @@ class AvailableSlots
 	end
 
 	def closest_appointment(arg)
+		secs = find_seconds(arg)
+		close_app = nil
+		time_difference = SECS_IN_DAY
+		parsed_file.each do |slot|
+			slot_time = find_seconds(slot['time'])
+			close_app = slot if (slot_time - secs).abs < time_difference
+			time_difference = (slot_time - secs).abs if (slot_time - secs).abs < time_difference
+		end
+		return close_app
 	end
 
 	private
 
 	ROOT_PATH = File.join(File.dirname(__FILE__), '..')
+	SECS_IN_DAY = 86400
+
+	def find_seconds(t)
+		time = t.split(":")
+		secs = (time[0].to_i*3600) + (time[1].to_i*60) + time[2].to_i
+	end
 
 end
