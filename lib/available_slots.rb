7cd394e6c90f1cf9e16dd2cf49a1e86cb4dd3c2e
MaxProvin <maxprovin01@gmail.com>
2016-11-20 14:50:30 +0000

    Available slots object is initialized with available_slots.json

--- lib/available_slots.rb
+++ lib/available_slots.rb
@@ -1,2 +1,9 @@
 class AvailableSlots
+
+	attr_reader :available
+
+	def initialize
+		@available = File.read("../assets/availability_slots.json")
+	end
+
 end
--- spec/available_slots_spec.rb
+++ spec/available_slots_spec.rb
@@ -1,5 +1,14 @@
 require 'available_slots'
 
+Dir.chdir(File.dirname(__FILE__))
+
 describe AvailableSlots do
+
+	let(:slots) { described_class.new }
+	json_file = File.read("../assets/availability_slots.json")
+
+	it 'is initialized with all the available times' do
+		expect(slots.available).to eq json_file
+	end
 	
 end
