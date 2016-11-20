7d9124666e3dd65b4a05c7f313ce28095b62a547
MaxProvin <maxprovin01@gmail.com>
2016-11-20 13:03:17 +0000

    ARGV tested for no. of arguments

--- lib/availability.rb
+++ lib/availability.rb
@@ -1,2 +1,14 @@
 class Availability
+
+	attr_reader :time
+
+	def initialize
+		raise ARG_ERROR if ARGV.length != 1
+		@time = ARGV
+	end
+
+	private
+
+	ARG_ERROR = "Format: availability.rb hh:mm"
+
 end
--- spec/availability_spec.rb
+++ spec/availability_spec.rb
@@ -1,11 +1,21 @@
-require 'availability'
+# require 'availability'
 
 describe Availability do
 	
 	let(:availability) { described_class.new }
 
-	it 'initialized with ARGV from command line' do
-		expect(availability.time).to be_truthy
+	ARG_ERROR = "Format: availability.rb hh:mm"
+
+	context 'Command Line Argument' do
+		it 'initialized with ARGV from command line' do
+			stub_const("ARGV", ['12:15'])
+			expect(availability.time).not_to eq []
+		end
+
+		it 'Error if more/less than 1 argument provided' do
+			stub_const("ARGV", ['12:15', 'hello'])
+			expect{availability}.to raise_error ARG_ERROR
+		end
 	end
 
 end
