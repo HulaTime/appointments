a2a88790fa062389eec0a767ab16c5b2050b44d3
MaxProvin <maxprovin01@gmail.com>
2016-11-20 14:57:22 +0000

    explicitly defined root path for available slots

--- lib/available_slots.rb
+++ lib/available_slots.rb
@@ -3,7 +3,11 @@ class AvailableSlots
 	attr_reader :available
 
 	def initialize
-		@available = File.read("../assets/availability_slots.json")
+		@available = File.read(ROOT_PATH + "/assets/availability_slots.json")
 	end
+
+	private
+
+	ROOT_PATH = File.join(File.dirname(__FILE__), '..')
 
 end
