f0cab36c25c862242ad2836afb05b64bbe2d3460
MaxProvin <maxprovin01@gmail.com>
2016-11-21 09:36:11 +0000

    guard conditions for input time in place

--- lib/availability.rb
+++ lib/availability.rb
@@ -4,6 +4,7 @@ class Availability
 
 	def initialize
 		raise ARG_ERROR if ARGV.length != 1
+		raise FORMAT_ERROR if bad_format?
 		raise RANGE_ERROR if out_of_range?(ARGV[0])
 		@time = ARGV
 	end
@@ -15,6 +16,11 @@ class Availability
 	RANGE_ERROR = "Sorry, bookings are only available between 08:00 and 15:00"
 
 	def bad_format?
+		t = ARGV[0].split(':')
+		t.map! do |n|
+			return true if (n =~ /^[0-9]+$/) == nil
+		end
+		false
 	end
 
 	def find_seconds(t)
